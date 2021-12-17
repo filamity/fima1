297f46c497e756bd7baac53d48dda88d6f4666cc
Dylan K <kainth.dylan@gmail.com>
2021-12-17 16:36:31 +0000

    added github logo in navbar

--- components/global/Footer.js
+++ components/global/Footer.js
@@ -1,9 +1,21 @@
 import styles from "../../styles/global/Footer.module.css";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
 
 const Footer = () => {
   return (
     <div className={`${styles.footerwrap} shadow`}>
-      FI-MA1
+      <Box sx={{ pb: 2 }}>
+        FI-MA1
+      </Box>
+      <Box>
+        <GitHubIcon onClick={() => {
+    window.open("https://github.com/filamity/fima1");
+  }} fontSize="large" className={styles.github} />
+      </Box>
+      
+    
     </div>
   );
 };
