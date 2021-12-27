9f2c66f4f1ebf6a5261784794c16908b8a983b57
Filamity <filamity@gmail.com>
2021-12-27 18:45:50 +0000

    minor fix

--- components/global/MathMark.js
+++ components/global/MathMark.js
@@ -1,25 +1,22 @@
 /* completely plagiarised from https://levelup.gitconnected.com/adding-katex-and-markdown-in-react-7b70694004ef */
 
-import React from 'react';
-import ReactMarkdown from 'react-markdown';
-import RemarkMathPlugin from 'remark-math';
+import ReactMarkdown from "react-markdown";
+import RemarkMathPlugin from "remark-math";
 
-import 'katex/dist/katex.min.css';
-import { BlockMath, InlineMath } from 'react-katex';
+import "katex/dist/katex.min.css";
+import { BlockMath, InlineMath } from "react-katex";
 
 const _mapProps = (props) => ({
   ...props,
   escapeHtml: false,
-  plugins: [
-    RemarkMathPlugin
-  ],
+  plugins: [RemarkMathPlugin],
   renderers: {
     ...props.renderers,
     math: ({ value }) => <BlockMath>{value}</BlockMath>,
-    inlineMath: ({ value }) => <InlineMath>{value}</InlineMath>
-  }
+    inlineMath: ({ value }) => <InlineMath>{value}</InlineMath>,
+  },
 });
 
 const Markdown = (props) => <ReactMarkdown {..._mapProps(props)} />;
 
-export default Markdown; 
+export default Markdown;
